331c230fec2a076948f51084d02fee536110713e
tian <xiaoliang.tian@gmail.com>
2019-06-13 18:02:18 +0800

    support aksk env

--- auth_test.go
+++ auth_test.go
@@ -1,9 +1,10 @@
-package auth
+package auth_test
 
 import (
 	"net/http"
 	"testing"
 
+	"github.com/huaweicse/auth"
 	hws_cloud "github.com/huaweicse/auth/third_party/forked/datastream/aws"
 	"github.com/stretchr/testify/assert"
 )
@@ -14,27 +15,27 @@ func Test_Auth(t *testing.T) {
 	shaaksk := "02a8ceaa95db6653c6f033759774d3bcc01be6b97da1c4ce218ef2451630eeb5"
 	project := "project1"
 
-	sign, err := GetSignFunc(ak, sk, project)
+	sign, err := auth.GetSignFunc(ak, sk, project)
 	assert.NoError(t, err)
 	r, err := http.NewRequest("GET", "http://127.0.0.1:8080", nil)
 	r.Header = nil
 	assert.NoError(t, err)
 	err = sign(r)
 	assert.NoError(t, err)
-	assert.Equal(t, r.Header.Get(HeaderServiceAk), ak)
-	assert.Equal(t, r.Header.Get(HeaderServiceShaAKSK), shaaksk)
-	assert.Equal(t, r.Header.Get(HeaderServiceProject), project)
+	assert.Equal(t, r.Header.Get(auth.HeaderServiceAk), ak)
+	assert.Equal(t, r.Header.Get(auth.HeaderServiceShaAKSK), shaaksk)
+	assert.Equal(t, r.Header.Get(auth.HeaderServiceProject), project)
 	assert.NotEmpty(t, r.Header.Get(hws_cloud.HeaderAuthorization))
 
-	sign, err = GetShaAKSKSignFunc(ak, sk, project)
+	sign, err = auth.GetShaAKSKSignFunc(ak, sk, project)
 	r.Header = nil
 	assert.NoError(t, err)
 	r, err = http.NewRequest("GET", "http://127.0.0.1:8080", nil)
 	assert.NoError(t, err)
 	err = sign(r)
 	assert.NoError(t, err)
-	assert.Equal(t, r.Header.Get(HeaderServiceAk), ak)
-	assert.Equal(t, r.Header.Get(HeaderServiceShaAKSK), shaaksk)
-	assert.Equal(t, r.Header.Get(HeaderServiceProject), project)
+	assert.Equal(t, r.Header.Get(auth.HeaderServiceAk), ak)
+	assert.Equal(t, r.Header.Get(auth.HeaderServiceShaAKSK), shaaksk)
+	assert.Equal(t, r.Header.Get(auth.HeaderServiceProject), project)
 	assert.Empty(t, r.Header.Get(hws_cloud.HeaderAuthorization))
 }
--- examples/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import _ "github.com/huaweicse/auth/adaptor/gochassis"
-
-func main() {
-
-}
